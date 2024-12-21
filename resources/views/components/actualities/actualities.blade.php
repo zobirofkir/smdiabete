8fb62adb302c08c3b2320cb8fec1e9365955e015
zobirofkir <zobirofkir19@gmail.com>
2024-12-21 23:30:33 +0100

    add whitespace-nowrap

--- resources/views/components/actualities/actualities.blade.php
+++ resources/views/components/actualities/actualities.blade.php
@@ -1,23 +1,22 @@
 <section class="bg-white py-12">
     <div class="container mx-auto px-6">
-        <div class="grid grid-cols-1 lg:grid-cols-2 gap-8 items-center">
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
 
             <div class="flex justify-center">
                 <img
                     src="{{asset('assets/images/affiche/affiche.jpeg')}}"
                     alt="Actualities Image"
-                    class="w-80 lg:w-96 shadow-lg rounded-lg"
+                    class="w-80 md:w-96 shadow-lg rounded-lg"
                 />
             </div>
 
-
-            <div class="text-center lg:text-left text-blue-900 space-y-6">
+            <div class="text-center md:text-left text-blue-900 space-y-6">
                 <h2 class="text-2xl md:text-4xl font-semibold">
                     Inscrivez-vous ! Plus que
                 </h2>
 
                 <!-- Countdown Timer -->
-                <div id="countdown" class="flex justify-center lg:justify-start space-x-4 text-center">
+                <div id="countdown" class="flex justify-center md:justify-start space-x-4 text-center">
                     <div>
                         <p id="days" class="text-3xl md:text-5xl font-bold">00</p>
                         <p class="text-sm md:text-base">JOURS</p>
@@ -44,14 +43,14 @@
                 </p>
 
                 <!-- Buttons -->
-                <div class="flex justify-center lg:justify-start space-x-4 mt-6">
-                    <button class="bg-blue-700 hover:bg-blue-900 text-white font-medium py-2 px-4 rounded-lg">
+                <div class="flex justify-center md:justify-start space-x-4 mt-6">
+                    <button class="bg-blue-700 hover:bg-blue-900 text-white font-medium py-2 px-4 rounded-lg whitespace-nowrap">
                         Inscription
                     </button>
-                    <button class="bg-blue-700 hover:bg-blue-900 text-white font-medium py-2 px-4 rounded-lg">
+                    <button class="bg-blue-700 hover:bg-blue-900 text-white font-medium py-2 px-4 rounded-lg whitespace-nowrap">
                         Visiter les stands
                     </button>
-                    <button class="bg-blue-700 hover:bg-blue-900 text-white font-medium py-2 px-4 rounded-lg">
+                    <button class="bg-blue-700 hover:bg-blue-900 text-white font-medium py-2 px-4 rounded-lg whitespace-nowrap">
                         Bureau
                     </button>
                 </div>
