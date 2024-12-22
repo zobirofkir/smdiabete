7e9053ffa33e183bf46dbc12d69df934946f752b
zobirofkir <zobirofkir19@gmail.com>
2024-12-22 01:11:03 +0100

    create sponsors

---
+++ resources/views/components/sponsors/sponsors.blade.php
@@ -0,0 +1,31 @@
+<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 container mx-auto">
+    <div class="bg-white rounded-lg shadow-md p-4 text-center">
+        <img src="https://www.myonlinevent.com/SMSNA/public/images/sponsor/novartisstand1.png" alt="Novartis Logo" class="mx-auto mb-4">
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Visiter le Stand</button>
+    </div>
+
+    <div class="bg-white rounded-lg shadow-md p-4 text-center">
+        <img src="https://www.myonlinevent.com/SMSNA/public/images/stand/pfizerstand10.jpg" alt="Pfizer Logo" class="mx-auto mb-4">
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Visiter le Stand</button>
+    </div>
+
+    <div class="bg-white rounded-lg shadow-md p-4 text-center">
+        <img src="https://www.myonlinevent.com/SMSNA/public/images/sponsor/servierstand.png" alt="Servier Logo" class="mx-auto mb-4">
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Visiter le Stand</button>
+    </div>
+
+    <div class="bg-white rounded-lg shadow-md p-4 text-center">
+        <img src="https://www.myonlinevent.com/SMSNA/public/images/phistand12.jpg" alt="Pharmaceutical Institute Logo" class="mx-auto mb-4">
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Visiter le Stand</button>
+    </div>
+
+    <div class="bg-white rounded-lg shadow-md p-4 text-center">
+        <img src="https://www.myonlinevent.com/SMSNA/public/images/menarinistand12.jpg" alt="Phi Menarini Logo" class="mx-auto mb-4">
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Visiter le Stand</button>
+    </div>
+
+    <div class="bg-white rounded-lg shadow-md p-4 text-center">
+        <img src="https://www.myonlinevent.com/SMSNA/public/images/AFRICPHARstand1.jpg" alt="Afric Phar Logo" class="mx-auto mb-4">
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Visiter le Stand</button>
+    </div>
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -28,4 +28,11 @@
         </h1>
     </div>
     @include('components.videos.videos')
+
+    <div class="text-center mt-10">
+        <h1 class="text-3xl md:text-4xl font-extrabold text-blue-900 mb-4 transition-all duration-300 transform hover:text-blue-600 hover:scale-105">
+            Nos Sponsors
+        </h1>
+    </div>
+    @include('components.sponsors.sponsors')
 </x-app-layout>
