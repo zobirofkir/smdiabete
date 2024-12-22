8cda6108029362ec60067ddfe3d8ac36bd3367cf
zobirofkir <zobirofkir19@gmail.com>
2024-12-22 14:42:40 +0100

    fix error from footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,6 +1,6 @@
-<footer class="bg-light-blue text-white py-8 mt-auto">
-    <div class="container mx-auto text-center md:mx-0 px-10">
-        <h1 class="text-2xl font-bold mb-4">{{ config('app.name') }}</h1>
-        <p>Copyright &copy; <script>document.write(new Date().getFullYear());</script> {{ config('app.name') }}. All rights reserved.</p>
+<footer class="bg-light-blue text-white py-8">
+    <div class="flex flex-col justify-center items-center md:px-0 px-10">
+        <h1 class="text-2xl font-bold mb-4 text-center">{{ config('app.name') }}</h1>
+        <p class="text-center">Copyright &copy; <script>document.write(new Date().getFullYear());</script> {{ config('app.name') }}. All rights reserved.</p>
     </div>
 </footer>
