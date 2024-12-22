444507498d25940dd5885277015217ca93b8236e
zobirofkir <zobirofkir19@gmail.com>
2024-12-22 01:13:42 +0100

    finalisé sponsors

--- resources/views/components/sponsors/sponsors.blade.php
+++ resources/views/components/sponsors/sponsors.blade.php
@@ -1,31 +1,40 @@
-<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 container mx-auto">
-    <div class="bg-white rounded-lg shadow-md p-4 text-center">
-        <img src="https://www.myonlinevent.com/SMSNA/public/images/sponsor/novartisstand1.png" alt="Novartis Logo" class="mx-auto mb-4">
-        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Visiter le Stand</button>
+<section class="container mx-auto">
+    <div class="text-center">
+        <h1>
+            Les membres de la Société Marocaine du Système Nerveux Autonome remercient les laboratoires pharmaceutiques et les sociétés du matériel médical pour leur soutien moral et financier ainsi qu’à leur confiance. Cette participation a contribué à la réalisation de cette importante rencontre scientifique.
+        </h1>
     </div>
 
-    <div class="bg-white rounded-lg shadow-md p-4 text-center">
-        <img src="https://www.myonlinevent.com/SMSNA/public/images/stand/pfizerstand10.jpg" alt="Pfizer Logo" class="mx-auto mb-4">
-        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Visiter le Stand</button>
-    </div>
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
 
-    <div class="bg-white rounded-lg shadow-md p-4 text-center">
-        <img src="https://www.myonlinevent.com/SMSNA/public/images/sponsor/servierstand.png" alt="Servier Logo" class="mx-auto mb-4">
-        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Visiter le Stand</button>
-    </div>
+        <div class="bg-white rounded-lg shadow-md p-4 text-center">
+            <img src="https://www.myonlinevent.com/SMSNA/public/images/sponsor/novartisstand1.png" alt="Novartis Logo" class="mx-auto mb-4">
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Visiter le Stand</button>
+        </div>
 
-    <div class="bg-white rounded-lg shadow-md p-4 text-center">
-        <img src="https://www.myonlinevent.com/SMSNA/public/images/phistand12.jpg" alt="Pharmaceutical Institute Logo" class="mx-auto mb-4">
-        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Visiter le Stand</button>
-    </div>
+        <div class="bg-white rounded-lg shadow-md p-4 text-center">
+            <img src="https://www.myonlinevent.com/SMSNA/public/images/stand/pfizerstand10.jpg" alt="Pfizer Logo" class="mx-auto mb-4">
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Visiter le Stand</button>
+        </div>
 
-    <div class="bg-white rounded-lg shadow-md p-4 text-center">
-        <img src="https://www.myonlinevent.com/SMSNA/public/images/menarinistand12.jpg" alt="Phi Menarini Logo" class="mx-auto mb-4">
-        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Visiter le Stand</button>
-    </div>
+        <div class="bg-white rounded-lg shadow-md p-4 text-center">
+            <img src="https://www.myonlinevent.com/SMSNA/public/images/sponsor/servierstand.png" alt="Servier Logo" class="mx-auto mb-4">
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Visiter le Stand</button>
+        </div>
 
-    <div class="bg-white rounded-lg shadow-md p-4 text-center">
-        <img src="https://www.myonlinevent.com/SMSNA/public/images/AFRICPHARstand1.jpg" alt="Afric Phar Logo" class="mx-auto mb-4">
-        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Visiter le Stand</button>
+        <div class="bg-white rounded-lg shadow-md p-4 text-center">
+            <img src="https://www.myonlinevent.com/SMSNA/public/images/phistand12.jpg" alt="Pharmaceutical Institute Logo" class="mx-auto mb-4">
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Visiter le Stand</button>
+        </div>
+
+        <div class="bg-white rounded-lg shadow-md p-4 text-center">
+            <img src="https://www.myonlinevent.com/SMSNA/public/images/menarinistand12.jpg" alt="Phi Menarini Logo" class="mx-auto mb-4">
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Visiter le Stand</button>
+        </div>
+
+        <div class="bg-white rounded-lg shadow-md p-4 text-center">
+            <img src="https://www.myonlinevent.com/SMSNA/public/images/AFRICPHARstand1.jpg" alt="Afric Phar Logo" class="mx-auto mb-4">
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Visiter le Stand</button>
+        </div>
     </div>
-</div>
+</section>
