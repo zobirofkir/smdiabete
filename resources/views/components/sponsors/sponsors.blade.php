2915b77eaf434b6e1f417661527a574d64d64b65
zobirofkir <zobirofkir19@gmail.com>
2024-12-22 10:28:09 +0100

    add sponsors images

--- resources/views/components/sponsors/sponsors.blade.php
+++ resources/views/components/sponsors/sponsors.blade.php
@@ -1,4 +1,4 @@
-<section class=" px-8 md:px-16 ">
+<section class="px-8 md:px-16">
     <div class="text-center">
         <h1>
             Les membres de la Société Marocaine du Système Nerveux Autonome remercient les laboratoires pharmaceutiques et les sociétés du matériel médical pour leur soutien moral et financier ainsi qu’à leur confiance. Cette participation a contribué à la réalisation de cette importante rencontre scientifique.
@@ -6,14 +6,19 @@
     </div>
 
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-
-        @foreach (range(1, 6) as $item)
+        @foreach (range(1, 24) as $item)
             <div class="bg-white rounded-lg shadow-md p-4 text-center">
-                <img src="https://www.myonlinevent.com/SMSNA/public/images/sponsor/novartisstand1.png" alt="Novartis Logo" class="mx-auto mb-4 hover:rotate-6 transition-transform duration-300 ease-in-out">
-                <button class="bg-sky-500 hover:bg-sky-700 text-white font-bold py-2 px-4 rounded">Visiter le Stand</button>
+                <div class="flex justify-center items-center h-32 w-full overflow-hidden bg-white rounded-lg">
+                    <img
+                        src="{{asset('assets/images/sponsors/sponsor-' . $item . '.jpeg')}}"
+                        alt="Sponsor Logo"
+                        class="object-contain h-full w-auto"
+                    >
+                </div>
+                <button class="bg-sky-500 hover:bg-sky-700 text-white font-bold py-2 px-4 rounded mt-10">
+                    Visiter le Stand
+                </button>
             </div>
         @endforeach
-
-
     </div>
 </section>
