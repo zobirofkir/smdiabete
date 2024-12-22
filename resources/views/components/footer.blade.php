2181b9b567c380bc55bfcc13bd60488c53d8c2b6
zobirofkir <zobirofkir19@gmail.com>
2024-12-22 02:05:02 +0100

    get full year and update blue to sky color in welcome page

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="bg-light-blue text-white py-8">
     <div class="container mx-auto text-center">
         <h1 class="text-2xl font-bold mb-4">{{ config('app.name') }}</h1>
-        <p>Copyright &copy; 2023 {{ config('app.name') }}. All rights reserved.</p>
+        <p>Copyright &copy; <script>document.write(new Date().getFullYear());</script> {{ config('app.name') }}. All rights reserved.</p>
         <p>Privacy Policy | Terms of Service</p>
     </div>
 </footer>
