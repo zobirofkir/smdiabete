d79875bd63b91593294f2a64ae712e08fd1c5099
zobirofkir <zobirofkir19@gmail.com>
2024-12-23 01:35:33 +0100

    create inscription schema and request

---
+++ app/Http/Requests/InscriptionRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class InscriptionRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            "firstname" => "required|string",
+            "lastname" => "required|string",
+            "ville" => "required|string",
+            "secteur" => "required|string",
+            "phone" => "required|string",
+            "email" => "required|string",
+            "adresse" => "required|string",
+            "objectif" => "required|string",
+        ];
+    }
+}
