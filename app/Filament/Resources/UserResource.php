3c981214d8ade6aeeba7a17290f304a4dc077db2
zobirofkir <zobirofkir19@gmail.com>
2024-12-23 19:36:26 +0100

    create new group for user resource

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -17,7 +17,7 @@ class UserResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
-    protected static ?string $navigationGroup = 'Inscriptions';
+    protected static ?string $navigationGroup = 'Users';
 
     public static function form(Form $form): Form
     {
