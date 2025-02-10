be8e0caffda979861c7544135ba40b27794326ea
zobirofkir <zobirofkir19@gmail.com>
2025-02-10 11:01:48 +0100

    updare inscription request messages

--- app/Http/Requests/InscriptionRequest.php
+++ app/Http/Requests/InscriptionRequest.php
@@ -58,29 +58,55 @@ class InscriptionRequest extends FormRequest
     public function messages(): array
     {
         return [
-            'rib_pdf.required' => 'Le fichier PDF du RIB est obligatoire pour le paiement par virement.',
-            'rib_pdf.mimes' => 'Le fichier doit être un PDF valide.',
-            'rib_pdf.max' => 'Le fichier PDF ne doit pas dépasser 2 Mo.',
-            'rib_pdf.file' => 'Le fichier PDF doit avoir une extension PDF.',
-            'rib_pdf.required_with' => 'Le fichier PDF du RIB est obligatoire pour le paiement par virement.',
-            'rib_pdf.required_with_all' => 'Le fichier PDF du RIB est obligatoire pour le paiement par virement.',
             'firstname.required' => 'Le prénom est requis.',
-            'lastname.required' => 'Le nom de famille est requis.',
-            'phone.required' => 'Le numéro de tелефone est requis.',
-            'email.required' => 'L\'email est requis.',
+            'firstname.string' => 'Le prénom doit être une chaîne de caractères.',
+            'firstname.max' => 'Le prénom ne peut pas dépasser 255 caractères.',
+
+            'lastname.required' => 'Le nom est requis.',
+            'lastname.string' => 'Le nom doit être une chaîne de caractères.',
+            'lastname.max' => 'Le nom ne peut pas dépasser 255 caractères.',
+
+            'phone.required' => 'Le numéro de téléphone est requis.',
+            'phone.string' => 'Le numéro de téléphone doit être une chaîne de caractères.',
+            'phone.max' => 'Le numéro de téléphone ne peut pas dépasser 15 caractères.',
+
+            'email.email' => 'L\'email doit être une adresse email valide.',
+            'email.max' => 'L\'email ne peut pas dépasser 255 caractères.',
+            'email.unique' => 'Cet email est déjà utilisé.',
+
             'secteur.required' => 'Le secteur est requis.',
+            'secteur.string' => 'Le secteur doit être une chaîne de caractères.',
+            'secteur.in' => 'Le secteur doit être soit "public", soit "privé".',
+
             'type.required' => 'Le type est requis.',
-            'specialite.required' => 'La specialité est requise.',
+            'type.string' => 'Le type doit être une chaîne de caractères.',
+            'type.in' => 'Le type doit être soit "specialiste", soit "resident".',
+
+            'specialite.required' => 'La spécialité est requise.',
+            'specialite.string' => 'La spécialité doit être une chaîne de caractères.',
+            'specialite.max' => 'La spécialité ne peut pas dépasser 255 caractères.',
+            'specialite.function' => 'Veuillez entrer votre spécialité si vous sélectionnez "Autres".',
+
+            'other_specialite.string' => 'La spécialité "Autres" doit être une chaîne de caractères.',
+            'other_specialite.max' => 'La spécialité "Autres" ne peut pas dépasser 255 caractères.',
+
             'ville.required' => 'La ville est requise.',
-            'departure_date.required' => 'La date de sortie est requise.',
-            'payment_method.required' => 'Le moyen de paiement est requis.',
-            'payment_method.in' => 'Le moyen de paiement doit être "laboratoire", "virement" ou "invite".',
-            'email.unique' => 'Cet email est deja utilisé.',
-            'phone.unique' => 'Cet numéro de tелефone est deja utilisé.',
-            'rib_pdf.required_with_all' => 'Le fichier PDF du RIB est obligatoire pour le paiement par virement.',
-            'rib_pdf.required_with' => 'Le fichier PDF du RIB est obligatoire pour le paiement par virement.',
-            'rib_pdf.required_without' => 'Le fichier PDF du RIB est obligatoire pour le paiement par virement.',
-            'rib_pdf.required_without_all' => 'Le fichier PDF du RIB est obligatoire pour le paiement par virement.',
+            'ville.string' => 'La ville doit être une chaîne de caractères.',
+            'ville.max' => 'La ville ne peut pas dépasser 255 caractères.',
+
+            'arrival_date.required' => 'La date d\'arrivée est requise.',
+            'arrival_date.date' => 'La date d\'arrivée doit être une date valide.',
+
+            'departure_date.required' => 'La date de départ est requise.',
+            'departure_date.date' => 'La date de départ doit être une date valide.',
+
+            'payment_method.required' => 'Le mode de paiement est requis.',
+            'payment_method.string' => 'Le mode de paiement doit être une chaîne de caractères.',
+            'payment_method.in' => 'Le mode de paiement doit être "laboratoire", "virement", ou "invite".',
+
+            'rib_pdf.required' => 'Le fichier RIB est requis pour le mode de paiement "virement".',
+            'rib_pdf.file' => 'Le fichier RIB doit être un fichier.',
+            'rib_pdf.mimes' => 'Le fichier RIB doit être au format PDF.',
         ];
     }
 }
