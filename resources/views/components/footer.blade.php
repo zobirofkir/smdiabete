425fe397d424a928e39d4d22e541b1bcd66aebeb
zobirofkir <zobirofkir19@gmail.com>
2025-02-11 15:46:36 +0100

    update

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -2,5 +2,22 @@
     <div class="flex flex-col justify-center items-center md:px-0 px-10">
         <h1 class="text-2xl font-bold mb-4 text-center">{{ config('app.name') }}</h1>
         <p class="text-center">Copyright &copy; <script>document.write(new Date().getFullYear());</script> {{ config('app.name') }}. Tous droits réservés.</p>
+        <div class="flex items-center space-x-4">
+            <a href="https://www.youtube.com/@SMD81023" class="text-white hover:text-gray-200">
+                <i class="fab fa-youtube"></i>
+            </a>
+            <a href="https://web.facebook.com/profile.php?id=61553634816576" class="text-white hover:text-gray-200">
+                <i class="fab fa-facebook-f"></i>
+            </a>
+            <a href="#" class="text-white hover:text-gray-200">
+                <i class="fab fa-twitter"></i>
+            </a>
+            <a href="#" class="text-white hover:text-gray-200">
+                <i class="fab fa-linkedin-in"></i>
+            </a>
+            <a href="#" class="text-white hover:text-gray-200">
+                <i class="fab fa-instagram"></i>
+            </a>
+        </div>
     </div>
 </footer>
