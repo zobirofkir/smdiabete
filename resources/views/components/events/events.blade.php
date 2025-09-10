1a93a4390738aa7ccf5bc194f88864c3b6e33746
csw <zobirofkir19@gmail.com>
2025-09-10 20:41:34 +0100

    add all images in the same size

--- resources/views/components/events/events.blade.php
+++ resources/views/components/events/events.blade.php
@@ -1,6 +1,7 @@
-<div class="px-4 sm:px-6 lg:px-8">
+<div class="px-4 sm:px-6 lg:px-8 my-10">
     <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
-        <img src="{{asset('assets/images/events/last-event-1.jpeg')}}" alt="Event Affiche" class="w-full h-auto object-contain">
-        <img src="{{asset('assets/images/events/last-event-2.jpeg')}}" alt="Event Affiche" class="w-full h-auto object-contain">
+        <img src="{{asset('assets/images/events/last-event-1.jpeg')}}" alt="Event Affiche" class="w-full h-[100%] object-cover">
+        <img src="{{asset('assets/images/events/last-event-2.jpeg')}}" alt="Event Affiche" class="w-full h-[100%] object-cover">
+        <img src="{{asset('assets/images/events/last-event-4.jpeg')}}" alt="Event Affiche" class="w-full h-[100%] object-cover">
     </div>
 </div>
