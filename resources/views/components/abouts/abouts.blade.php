dbde0ee4fbe2e8ffd4b4929eac91d2c35f6d2962
csw <zobirofkir19@gmail.com>
2025-09-10 22:07:22 +0100

    update

--- resources/views/components/abouts/abouts.blade.php
+++ resources/views/components/abouts/abouts.blade.php
@@ -81,10 +81,8 @@
                         <div class="p-6 space-y-6">
                             <!-- President -->
                             <div class="text-center p-4 bg-gradient-to-r from-blue-50 to-emerald-50 rounded-2xl border-2 border-blue-200">
-                                <div class="w-16 h-16 bg-gradient-to-r from-blue-600 to-emerald-600 rounded-full mx-auto mb-3 flex items-center justify-center">
-                                    <svg class="w-8 h-8 text-white" fill="currentColor" viewBox="0 0 20 20">
-                                        <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd"></path>
-                                    </svg>
+                                <div class="w-20 h-20 mx-auto mb-3 rounded-full overflow-hidden border-4 border-white shadow-lg">
+                                    <img src="{{ asset('assets/images/sonia/sonia.jpg') }}" alt="Dr Sonia ABAHOU" class="w-full h-full object-cover">
                                 </div>
                                 <h3 class="text-lg font-bold text-blue-800 mb-1">Présidente</h3>
                                 <p class="text-gray-700 font-semibold">Dr Sonia ABAHOU</p>
