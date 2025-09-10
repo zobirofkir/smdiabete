f6d697c7e599759599b72b73accda1b8d0ae09dc
csw <zobirofkir19@gmail.com>
2025-09-10 20:26:46 +0100

    create contact request

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,24 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ContactRequest;
 use App\Models\Contact;
 use Illuminate\Http\Request;
 
 class ContactController extends Controller
 {
-    public function store(Request $request)
+    public function store(ContactRequest $request)
     {
-        $request->validate([
-            'name' => 'required|string|max:255',
-            'email' => 'required|email|max:255',
-            'message' => 'required|string'
-        ]);
-
-        Contact::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'message' => $request->message
-        ]);
+        Contact::create($request->validated());
 
         return back()->with('success', 'Votre message a été envoyé avec succès!');
     }
