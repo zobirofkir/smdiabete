70da4f5269d4f6a0de4cf09dfe49ea5ed9cdd1fb
csw <zobirofkir19@gmail.com>
2026-01-13 12:02:40 +0100

    fix error

--- resources/views/components/inscription/inscription-header.blade.php
+++ resources/views/components/inscription/inscription-header.blade.php
@@ -6,7 +6,9 @@
         </svg>
     </div>
     <h1 class="text-5xl md:text-6xl font-black bg-gradient-to-r from-gray-900 via-blue-800 to-indigo-800 bg-clip-text text-transparent mb-4 tracking-tight">INSCRIPTION</h1>
-    <p class="text-xl text-gray-600 max-w-2xl mx-auto leading-relaxed">Rejoignez notre événement médical d'excellence et participez à l'avenir de la médecine</p>
+    <p class="text-xl text-gray-600 max-w-2xl mx-auto leading-relaxed">
+        Rejoignez notre événement d’excellence et participez à l’avenir de la Diabétologie
+    </p>
     <div class="w-24 h-1 bg-gradient-to-r from-blue-600 to-indigo-600 mx-auto mt-6 rounded-full"></div>
 </div>
 
